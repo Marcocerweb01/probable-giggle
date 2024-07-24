059916b9c3254266fa9998da7fe026830bbb0f81
Marcocerweb01 <marcocerasaweb@gmail.com>
2024-07-24 18:33:27 +0200

    fix typo

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,12 +1,76 @@
-import { Suspense } from "react";
-import EditPromptComponent from "@components/EditPromptComponent";
+"use client";
+
+import { useState, useEffect, Suspense } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
 
 const EditPrompt = () => {
+  const router = useRouter();
+  const searchParamas= useSearchParams();
+  const promptId = searchParamas.get("id");
+
+
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+    useEffect(() => {
+        const getPromptDetails = async () =>{
+            const response = await fetch(`/api/prompt/${promptId}`);
+            const data = await response.json();
+            
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag,
+
+            })
+            console.log(data.prompt)
+        }
+        console.log(promptId);
+        if (promptId) getPromptDetails();
+    }, [promptId]);
+ 
+    const updatePrompt = async (e) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+        if (!promptId) return alert ("Prompt id non trovato")
+        try {
+          const response = await fetch(`/api/prompt/${promptId}`,{
+            method: "PATCH",
+            body: JSON.stringify({
+              prompt: post.prompt,
+              tag: post.tag
+            }),
+          });
+    
+          if (response.ok) {
+            router.push("/");
+          }
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setIsSubmitting(false);
+        }
+      };
+
+
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <EditPromptComponent />
-    </Suspense>
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+
   );
 };
 
-export default EditPrompt;
+const EditPromptsusp = () => {
+  return <Suspense>
+      <EditPrompt />
+  </Suspense>
+}
+
+export default EditPromptsusp;
