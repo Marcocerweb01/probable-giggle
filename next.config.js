d393138d154bef776898e9ca35230e367f3a2433
Marcocerweb01 <marcocerasaweb@gmail.com>
2024-07-25 11:36:08 +0200

    fix typo

--- next.config.js
+++ next.config.js
@@ -1,25 +1,24 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      // appDir: true,
-      missingSuspenseWithCSRBailout:false,
-      serverComponentsExternalPackages: ["mongoose"],
-      
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack: (config) => {
-      config.experiments = {
-        ...config.experiments,    
-        topLevelAwait: true,
-      },
-      config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
-      return config
-    },
-  
-  
-    
-  }
-  
-  module.exports = nextConfig
+  experimental: {
+    // appDir: true,
+    missingSuspenseWithCSRBailout: false,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
